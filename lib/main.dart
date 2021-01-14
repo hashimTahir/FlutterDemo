72b9c56fed5ed92da65cafd80d7598c5f738d7bb
hashim <hashimtahir@live.com>
2021-01-14 22:53:21 +0500

    Custom fonts and styling options added. Configuration goes in pubspec.yaml->Indentation is very important otherwise it wont work.
    Signed-off-by: hashim <hashimtahir@live.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-/*Scfold can act as a holder for bottom bar, navigation drawer
+/*Scfold can act as a wrapper for bottom bar, navigation drawer
 * floating action button etc.*/
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red[600],
         title: Text("Flutter Demo"),
         centerTitle: true,
       ),
       /*To nest widgets use child property*/
       body: Center(
-        child: Text("Any fool can write code that a computer can understand."
-            " Good programmers write code that humans can understand"),
+        child: Text(
+          "Any fool can write code that a computer can understand."
+          " Good programmers write code that humans can understand",
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+              letterSpacing: 2.0,
+              fontFamily: 'IndieFlower'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text("Sup"),
+        backgroundColor: Colors.red[600],
       ),
     ),
   ));
