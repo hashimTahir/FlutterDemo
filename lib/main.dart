5131c3a2f8190e210abb74db775191ba53ca23dc
hashim <hashimtahir@live.com>
2021-01-14 22:27:40 +0500

    Scafold holder and basic widget tree structure created.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
+/*Scfold can act as a holder for bottom bar, navigation drawer
+* floating action button etc.*/
   runApp(MaterialApp(
-    home: Text("Any fool can write code that a computer can understand."
-        " Good programmers write code that humans can understand"),
-
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Demo"),
+        centerTitle: true,
+      ),
+      /*To nest widgets use child property*/
+      body: Center(
+        child: Text("Any fool can write code that a computer can understand."
+            " Good programmers write code that humans can understand"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("Sup"),
+      ),
+    ),
   ));
 }
