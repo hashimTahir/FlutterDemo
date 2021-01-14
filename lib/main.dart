aeb47926bac27ce534431ac783d04f4f140d7a53
hashim <hashimtahir@live.com>
2021-01-14 23:21:39 +0500

    Network Image used for image loading. custom image request to be added later.
    Signed-off-by: hashim <hashimtahir@live.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
 /*Scfold can act as a wrapper for bottom bar, navigation drawer
 * floating action button etc.*/
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MaterialApp(home: HomeStatelessWidget()));
+}
+
+/*Statless Widget-> State of the widget canot change over time*/
+/*Stateful Widget-> State of the widget can change over time
+* Hot Reload -> on saving the build get called  and changes are applied.
+* */
+class HomeStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red[600],
         title: Text("Flutter Demo"),
@@ -12,15 +21,8 @@ void main() {
       ),
       /*To nest widgets use child property*/
       body: Center(
-        child: Text(
-          "Any fool can write code that a computer can understand."
-          " Good programmers write code that humans can understand",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey[600],
-              letterSpacing: 2.0,
-              fontFamily: 'IndieFlower'),
+        child:Image(
+          image: NetworkImage('https://cdn.pixabay.com/photo/2020/10/22/20/08/great-spotted-woodpecker-5677069_1280.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -28,6 +30,6 @@ void main() {
         child: Text("Sup"),
         backgroundColor: Colors.red[600],
       ),
-    ),
-  ));
+    );
+  }
 }
