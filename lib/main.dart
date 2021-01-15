12b5e9f557dd89bb2dff7a9c3068e9baa9ea84e2
hashim <hashimtahir@live.com>
2021-01-16 04:56:33 +0500

    Code clean. App Startup.
    Signed-off-by: hashim <hashimtahir@live.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,40 +6,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: QuotesList()));
-}
-
-class QuotesList extends StatefulWidget {
-  @override
-  _QuotesListState createState() => _QuotesListState();
-}
-
-class _QuotesListState extends State<QuotesList> {
-
-  List<String> hQuotesList = ["First, solve the problem. Then, write the code",
-    "Experience is the name everyone gives to their mistakes."
-        " In order to be irreplaceable, one must always be different"
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text(
-          'Quotes',
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent[200],
-      ),
-      body: Column(
-        children:
-        hQuotesList.map((quote) {
-          return Text(quote);
-        }).toList(),
-
-      ),
-    );
-  }
+  runApp(MaterialApp(home:));
 }
